472e46394c1fae699958f05b49f2dd466b69d48c
bonaime <bonaime@ipgp.fr>
2021-05-07 15:24:10 +0200

    ok

--- drone_photos_scan.py
+++ drone_photos_scan.py
@@ -4,7 +4,6 @@ import os
 import sys
 import re
 import logging
-import pickle
 import math
 import datetime
 from exif import Image as Image_exif
@@ -16,14 +15,11 @@ from skimage.filters import sobel
 from PIL import Image
 from geopy import distance
 import folium
-from folium.plugins import BeautifyIcon
-from gpxplotter import (
-    create_folium_map,
-    read_gpx_file,
-    add_segment_to_map,
-)
+from gpxplotter import create_folium_map
 
-dateFormat = '%Y:%m:%d %H:%M:%S'
+
+DATE_FORMAT = '%Y:%m:%d %H:%M:%S'
+
 
 class PhotoDrone:
     def __init__(self, directory, file):
@@ -54,7 +50,8 @@ class PhotoDrone:
 
             self.datetime_original = image_exif.datetime_original
             self.gps_timestamp = image_exif.datetime_original.split()[1]
-            self.epoch = datetime.datetime.strptime(self.datetime_original, dateFormat).timestamp()
+            self.epoch = datetime.datetime.strptime(
+                self.datetime_original, DATE_FORMAT).timestamp()
 
             self.change_distance = False
             self.change_direction = False
@@ -62,38 +59,41 @@ class PhotoDrone:
             self.distance = 0.0
             self.direction = 999
             self.direction_difference = 0.0
+            self.percent_distance_difference = 0
 
             self.is_blurry = False
-            self.first_image= False
+            self.first_image = False
+
+            self.vari_laplace_1 = None
+            self.maxi_laplace_1 = None
+            self.vari_sobel_1 = None
+            self.maxi_sobel_1 = None
+
 
 #            from IPython import embed; embed();sys.exit()
     def print(self):
         print('{: >20}\t{: >20}\t{: >20}\t{: >20}\t{: >10}\t{: >10}'
-        .format(self.filename, self.direction, self.distance, self.percent_distance_difference,
-        self.change_distance,self.change_direction ))
-        #print('{: >20}\t{: >20}\t{: >20}\t{}\t{: >10}\t{: >10}\t{: >10}\t{: >10}'
-        #.format(self.filename, self.distance, self.percent_distance_difference,
-        #self.change_distance, self.vari_laplace_1, self.maxi_laplace_1, self.vari_sobel_1, self.maxi_sobel_1))
+              .format(self.filename, self.direction, self.distance,
+                      self.percent_distance_difference,
+                      self.change_distance, self.change_direction))
 
     def compute_laplace_sobel(self):
 
         img = Image.open(self.filename)
 
         # square crops
-        square_size = 300
-        # im1.show()
 
         # square crops
         square_size = 400
         # Setting the points for cropped image
-        width, height = img.size
+        #width, height = img.size
 
         # Cropped image of above dimension
         # (It will not change orginal image)
         im1 = img.crop((0, 0, square_size, square_size))
-        im2 = img.crop((0, height-square_size, square_size, height))
-        im3 = img.crop((width-square_size, 0, width, square_size))
-        im4 = img.crop((width-square_size, height-square_size, width, height))
+        #im2 = img.crop((0, height-square_size, square_size, height))
+        #im3 = img.crop((width-square_size, 0, width, square_size))
+        #im4 = img.crop((width-square_size, height-square_size, width, height))
 
         # Edge detection
         edge_laplace_1 = laplace(im1, ksize=3)
@@ -111,6 +111,8 @@ class DronePhotosScan:
     def __init__(self, photos_directory, regex):
         self.photos_directory = photos_directory
         self.images = []
+        self.images_nb = None
+        self.average_distance = None
 
         if len([f for f in os.listdir(self.photos_directory) if not f.startswith('.')]) == 0:
             print('Directory {} is empty'.format(self.photos_directory))
@@ -125,7 +127,7 @@ class DronePhotosScan:
 
         # for each picture, create an photo_drone object
         for file in files:
-            self.images.append(PhotoDrone(self.photos_directory + '/',file))
+            self.images.append(PhotoDrone(self.photos_directory + '/', file))
 
     def compute_data(self):
         first_image = True
@@ -142,7 +144,8 @@ class DronePhotosScan:
                 #last_image.distance = 10000*pow(pow(delta_x, 2)+pow(delta_y, 2), 0.5)
                 #image.distance = 100000*pow(pow(image.delta_x, 2)+pow(image.delta_y, 2), 0.5)
                 coords_1 = (image.gps_longitude_dec, image.gps_latitude_dec)
-                coords_2 = (last_image.gps_longitude_dec, last_image.gps_latitude_dec)
+                coords_2 = (last_image.gps_longitude_dec,
+                            last_image.gps_latitude_dec)
 
                 image.distance = distance.geodesic(coords_1, coords_2).m
 
@@ -150,7 +153,7 @@ class DronePhotosScan:
                 #    (image.gps_timestamp_sec-last_image.gps_timestamp_sec)
                 last_image.direction = math.degrees(
                     math.atan2(image.delta_y, image.delta_x))
-                #print('{}\t{}\t{}\t{}'.format(
+                # print('{}\t{}\t{}\t{}'.format(
                 #    image.delta_x, image.delta_y, image.distance, image.direction))
             last_image = image
 
@@ -163,125 +166,119 @@ class DronePhotosScan:
         print("Average_distance " + str(self.average_distance))
 
     def print_values(self):
-        for im in self.images:
+        for image in self.images:
 
             print('filename\tdistance\tdirection{}')
-            print('{}\t{}\t{}'.format(im.filename, im.distance, im.direction))
+            print('{}\t{}\t{}'.format(image.filename, image.distance, image.direction))
 
-    def check_changes(self,direction_offset = 40,distance_difference_limit = 20):
+    def check_changes(self, direction_offset=40, distance_difference_limit=20):
         last_image = False
-        
+
         ##print('self.change_direction ')
         for image in self.images:
             image.percent_distance_difference = 100 * \
                 (image.distance - self.average_distance)/self.average_distance
-            
 
             if last_image:
-                image.direction_difference=image.direction - last_image.direction
+                image.direction_difference = image.direction - last_image.direction
 
                 # image.direction = 999 for the last image
-                if (((image.percent_distance_difference) < 0) & (abs(image.percent_distance_difference) > distance_difference_limit) & (image.direction < 999)) :
-                    #print('{}\t{}\t{}'.format(image.filename, image.distance, image.direction))
-                     #print(image.filename)
+                if ((image.percent_distance_difference) < 0
+                    and abs(image.percent_distance_difference) > distance_difference_limit
+                        and (image.direction < 999)):
 
                     image.change_distance = True
-                    image.is_blurry = True 
+                    image.is_blurry = True
                 else:
                     image.change_distance = False
 
-                
                 # image.direction = 999 for the last image
 
-                if abs(((image.direction_difference) > direction_offset) & (image.direction <999)) :
-
-                    #print("##########")
-                    #print('{}\t{}\t{}\t{}\t{}\t{}'.format(image.filename, image.distance, image.direction,image.direction_difference, direction_offset,abs(image.direction_difference)))
+                if abs(((image.direction_difference) > direction_offset) & (image.direction < 999)):
                     image.change_direction = True
-                    image.is_blurry = True 
+                    image.is_blurry = True
                 else:
                     image.change_direction = False
 
-                #### In case there is more than 10 secondes between images, remove change_direction and change_distance
+                # In case there is more than 10 secondes between images
+                # remove change_direction and change_distance
                 if abs(image.epoch - last_image.epoch) > 10:
                     last_image.change_direction = False
                     last_image.change_distance = False
-                    image.is_blurry = False 
+                    image.is_blurry = False
             else:
                 # première image de la série
                 image.first_image = True
 
             last_image = image
 
-
     def map_vectors(self):
-        X = []
-        Y = []
-        U = []
-        V = []
-        C = []
+        x_vector = []
+        y_vector = []
+        u_vector = []
+        v_vector = []
+        c_vector = []
 
         for img in self.images:
-            X.append(img.gps_longitude_dec)
-            Y.append(img.gps_latitude_dec)
-            if (img.distance > self.average_distance*0, 75):
-                U.append(math.cos(math.radians((img.direction)))*img.distance)
-                V.append(math.sin(math.radians((img.direction)))*img.distance)
-                C.append(0)
+            x_vector.append(img.gps_longitude_dec)
+            y_vector.append(img.gps_latitude_dec)
+            if img.distance > self.average_distance*0.75:
+                u_vector.append(math.cos(math.radians((img.direction)))*img.distance)
+                v_vector.append(math.sin(math.radians((img.direction)))*img.distance)
+                c_vector.append(0)
             else:
-                U.append(math.cos(math.radians((img.direction)))*img.distance)
-                V.append(math.sin(math.radians((img.direction)))*img.distance)
-                C.append(10)
+                u_vector.append(math.cos(math.radians((img.direction)))*img.distance)
+                v_vector.append(math.sin(math.radians((img.direction)))*img.distance)
+                c_vector.append(10)
 
-        Fig, Ax = plt.subplots()
-        Ax.quiver(X, Y, U, V, C, units='xy', scale=1)
+        #fig, ax = plt.subplots()
+        _, axis = plt.subplots()
+        axis.quiver(x_vector, y_vector, u_vector, v_vector, c_vector, units='xy', scale=1)
         #q = ax.quiver(X, Y, U, V, C, units='xy', scale=1)
 
         plt.grid()
         plt.show()
 
-
-
     def map(self):
 
-        the_map = create_folium_map(   zoom_start=3,max_zoom=50)
-        
+        the_map = create_folium_map(zoom_start=3, max_zoom=50)
+
         # Add custom base maps to folium
         basemaps = {
             'Google Maps': folium.TileLayer(
-                tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-                attr = 'Google',
-                name = 'Google Maps',
-                overlay = False,
-                control = True
+                tiles='https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Maps',
+                overlay=False,
+                control=True
             ),
             'Google Satellite': folium.TileLayer(
-                tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-                attr = 'Google',
-                name = 'Google Satellite',
-                overlay = True,
-                control = True
+                tiles='https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Satellite',
+                overlay=True,
+                control=True
             ),
             'Google Terrain': folium.TileLayer(
-                tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
-                attr = 'Google',
-                name = 'Google Terrain',
-                overlay = True,
-                control = True
+                tiles='https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Terrain',
+                overlay=True,
+                control=True
             ),
             'Google Satellite Hybrid': folium.TileLayer(
-                tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
-                attr = 'Google',
-                name = 'Google Satellite',
-                overlay = True,
-                control = True
+                tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Satellite',
+                overlay=True,
+                control=True
             ),
             'Esri Satellite': folium.TileLayer(
-                tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-                attr = 'Esri',
-                name = 'Esri Satellite',
-                overlay = True,
-                control = True
+                tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+                attr='Esri',
+                name='Esri Satellite',
+                overlay=True,
+                control=True
             )
         }
 
@@ -292,13 +289,13 @@ class DronePhotosScan:
         basemaps['Google Satellite Hybrid'].add_to(the_map)
         basemaps['Esri Satellite'].add_to(the_map)
 
-        
-        tiles_maps=[ 'openstreetmap','Stamen Terrain']
-        #tiles_maps=[ 'openstreetmap',''Cartodb Positron','Stamen Terrain','Stamen Toner','Stamen Watercolor']
-        for tm in tiles_maps:
-            folium.TileLayer(tm).add_to(the_map)
-    
-        data = []
+        tiles_maps = ['openstreetmap', 'Stamen Terrain']
+        # tiles_maps=[ 'openstreetmap',''Cartodb Positron',
+        # 'Stamen Terrain','Stamen Toner','Stamen Watercolor']
+        for tile in tiles_maps:
+            folium.TileLayer(tile).add_to(the_map)
+
+        #data = []
 #        for im in self.images:
 #            data.append(
 #                [im.gps_latitude_dec, im.gps_longitude_dec, im.distance])
@@ -306,23 +303,23 @@ class DronePhotosScan:
         folium.LayerControl(sortLayers=False).add_to(the_map)
 
         for image in self.images:
-            if (image.is_blurry == True):
-                color_image='darkred'
+            if image.is_blurry:
+                color_image = 'darkred'
             else:
                 color_image = 'green'
 
             folium.Marker(
-                location=[image.gps_latitude_dec,image.gps_longitude_dec],
+                location=[image.gps_latitude_dec, image.gps_longitude_dec],
                 popup=image.filename,
-                #color=color_image,
-#                icon=folium.Icon(color=color_image,icon='fas fa-camera')
-                icon=folium.Icon(color=color_image,icon='fa-map-pin')
-                
+                # color=color_image,
+                #                icon=folium.Icon(color=color_image,icon='fas fa-camera')
+                icon=folium.Icon(color=color_image, icon='fa-map-pin')
+
             ).add_to(the_map)
- 
+
         boundary = the_map.get_bounds()
         the_map.fit_bounds(boundary, padding=(3, 3))
- 
+
         the_map.save(self.photos_directory+'/carte.html')
         # sys.exit()
 
@@ -334,11 +331,12 @@ def main():
 
     parser.add_argument('-d', '--photos_directory', type=str, required=True,
                         help='The directory where drones pictures are. default is pwd')
-                        # default=os.getcwd())
+    # default=os.getcwd())
 
     parser.add_argument('-r', '--regex',  type=str, required=False,
-                        help='Regex expression to filter images. Default is "".*(jpg|jpeg|JPEG|JPG)"',
-                        default='".*(jpg|jpeg|JPEG|JPG)"')
+                        help='Regex expression to filter images. \
+                        Default is ".*(jpg|jpeg|JPEG|JPG)"',
+                        default='.*(jpg|jpeg|JPEG|JPG)')
 
     parser.add_argument('-v', '--verbose', default=False,
                         action="store_true", dest="verbose",
@@ -359,47 +357,23 @@ def main():
 
 #########################################################
 
-    # from_files = False
-    # from_files = True
-
-    # if from_files:
-    #    # print("Init")
-    #     project = DronePhotosScan(args.photos_directory, args.regex)
-    #     #print("Compute data")
-    #     project.compute_data()
-    #     # print("change_direction")
-    #     project.change_direction()
-    #     project.change_distance()
-    # # sauvegarde
-    #     with open('multi.obj', 'wb') as file_pi:
-    #         #        with open('multi.obj', 'wb') as file_pi:
-    #         pickle.dump(project, file_pi)
-    # else:
-    #     with open('multi.obj', 'rb') as file_pi:
-    #      #       with open('multi.obj', 'rb') as file_pi:
-    #         project = pickle.load(file_pi)
-
-
 
     print("Init")
     project = DronePhotosScan(args.photos_directory, args.regex)
     print("Compute data")
     project.compute_data()
     print("check_changes")
-    project.check_changes(direction_offset = 40,distance_difference_limit = 20)
+    project.check_changes(direction_offset=40, distance_difference_limit=20)
 
-
-
-   
- 
     print('{: ^50}\t{: ^10}\t{: ^10}\t{: ^10}\t{: ^10}\t{: ^8}\t{: ^8}'
           .format('file', 'distance', '%_dist_diff',
                   'direction', 'dir_diff', 'chg_dist', 'chg_dir'))
     for image in project.images:
        # image.compute_laplace_sobel()
-            print('{: ^50}\t{:>10.2f}\t{:>10.2f}\t{:>10.2f}\t{:>10.2f}\t{: ^8}\t{: ^8}'
-                .format(image.filename, image.distance, image.percent_distance_difference,
-                        image.direction, image.direction_difference, image.change_distance, image.change_direction))
+        print('{: ^50}\t{:>10.2f}\t{:>10.2f}\t{:>10.2f}\t{:>10.2f}\t{: ^8}\t{: ^8}'
+              .format(image.filename, image.distance, image.percent_distance_difference,
+                      image.direction, image.direction_difference,
+                      image.change_distance, image.change_direction))
 
     print('The following images may be blurry')
     print('{: ^20}\t{: ^10}\t{: ^10}\t{: ^10}\t{: ^10}\t{: ^8}\t{: ^8}'
@@ -407,17 +381,19 @@ def main():
                   'direction', 'dir_diff', 'chg_dist', 'chg_dir'))
     count = 0
     for image in project.images:
-        if image.is_blurry : 
+        if image.is_blurry:
             print('{: ^20}\t{:>10.2f}\t{:>10.2f}\t{:>10.2f}\t{:>10.2f}\t{: ^8}\t{: ^8}'
                   .format(image.file, image.distance, image.percent_distance_difference,
-                          image.direction, image.direction_difference, image.change_distance, image.change_direction))
-            count=count+1
+                          image.direction, image.direction_difference,
+                           image.change_distance, image.change_direction))
+            count = count+1
 
-    print(str(count)+ ' images may be blurry')
+    print(str(count) + ' images may be blurry')
 
     project.map()
 
    # from IPython import embed; embed()
+
 
 if __name__ == '__main__':
     main()
